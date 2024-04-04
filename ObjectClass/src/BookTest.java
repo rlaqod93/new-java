ff9f52c45bc740300f37528d04d3a90173d3296b
rlaqod93 <rlaqod93@naver.com>
2024-04-04 15:19:28 +0900

    2024/04/04

---
+++ ObjectClass/src/BookTest.java
@@ -0,0 +1,22 @@
+public class BookTest {
+    public static void main(String[] args) {
+        //정수 한개를 저장할 변수를 선언 하시오.
+        int a;
+
+        //한 권의 책 데이터를 저장하고 출력하시요. 6개의변수
+        String title = "자바";
+        int price = 30000;
+        String company = "패스트캠퍼스";
+        String author = "박매일";
+        int page = 700;
+        String isdn = "ISDB-11990011";
+        System.out.println(title+"\t"+price+"\t"+company+"\t"+author+"\t"+page+"\t"+isdn);
+
+        //책 한권을 저장할 변수를 선언 하시요.
+    }
+
+    public static void printBook(      ){
+        //public static void printBook(괄호 안에서 외부로부터 데이터를 받는다)
+    }
+
+}

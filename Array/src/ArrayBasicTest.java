491381ea0ebbb4b34a6737cde386e440dd991cc4
rlaqod93 <rlaqod93@naver.com>
2024-04-10 13:36:52 +0900

    2024/04/10

---
+++ Array/src/ArrayBasicTest.java
@@ -0,0 +1,13 @@
+public class ArrayBasicTest {
+    public static void main(String[] args) {
+        //정수 3개를 저장 할 변수를 선언하고 모두 10을 저장하세요.
+        //개별로 만들면 데이터의 수가 늘어나면 비효율적
+        //개별로 만들면 데이터 처리가 복잡
+        int a, b, c;
+        a = 10;
+        b = 10;
+        c = 10;
+        int sum  = a + b + c;
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ Array/src/ArrayMake.java
@@ -0,0 +1,9 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        //정수 5개를 저장 할 배열을 생성하시요.
+        //배열(object)
+        int[] a = new int[5];
+
+
+    }
+}
